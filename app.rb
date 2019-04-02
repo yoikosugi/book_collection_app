8eac019f75401238b162e79536b71c4e5b994324
yoshihiko sughimura <yoikosugi@gmail.com>
2019-04-02 16:15:03 +0900

    add database

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sqlite3'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: './db/books.db'
+)
+
+class Book < ActiveRecord::Base
+end
 
 get '/home' do
   @title = "蔵書管理"
@@ -14,5 +22,6 @@ end
 
 get '/list' do
   @title = "一覧"
+  @books = Book.all
   erb :list
 end
--- db/books.sql
+++ db/books.sql
@@ -1,7 +1,14 @@
-create table books(
+drop table if exists books;
+create table books (
   id integer primary key,
-  name text,
+  bookname text,
   author text,
   publisher text,
   points integer
 );
+
+insert into books (bookname, author, publisher, points) values ('蜘蛛の糸', '芥川龍之介', '旺文社', 5);
+insert into books (bookname, author, publisher, points) values ('rabbit,run', 'john updike', 'modern classics', 5);
+insert into books (bookname, author, publisher, points) values ('kumonoito', 'akutagawa', 'oubunnsya', 5);
+
+
