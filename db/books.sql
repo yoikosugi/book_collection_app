fdac155aa0f32f49785388732b77a137047c9481
yoshihiko sughimura <yoikosugi@gmail.com>
2019-04-03 15:16:24 +0900

    Remove dummy data from db

--- db/books.sql
+++ db/books.sql
@@ -6,7 +6,3 @@ create table books (
   publisher text,
   points integer
 );
-
-insert into books (bookname, author, publisher, points) values ('蜘蛛の糸', '芥川龍之介', '旺文社', 5);
-insert into books (bookname, author, publisher, points) values ('rabbit,run', 'john updike', 'modern classics', 5);
-insert into books (bookname, author, publisher, points) values ('kumonoito', 'akutagawa', 'oubunnsya', 5);
